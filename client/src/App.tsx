671b2977d9d626d3ff4d93e316d437d839832111
vedantvbarve <vedantvbarve@gmail.com>
2026-02-20 14:43:07 +0530

    New commit. Ignore

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 //import Login from "./views/Login";
 //import Main from "./views/Main";
+// import Main from "./views/Main";
 import RecordVerification from "./views/training_points/RecordVerification";
 // import TrainingPoints from "./views/training_points/TrainingPoints";
 
@@ -9,6 +10,7 @@ function App() {
   //return <Login />;
   // return <TrainingPoints/> 
   return<RecordVerification />;
+  // return <Main />;
 }
 
 export default App;
